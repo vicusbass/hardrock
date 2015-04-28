a7e19fd86850222556be6101affd78c2fe41c08a
vicusbass <vasile.pop@gmail.com>
2015-04-29 00:29:12 +0300

    added some styling

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -3,33 +3,35 @@ Template.home.rendered = function() {
 };
 
 Template.home.helpers({
-	settings: function() {
-		return {
-			position: "top",
-			limit: 5,
-			rules: [{
-				collection: 'Songs',
-				subscription: 'autocompleteArtists',
-				field: "artist",
-				template: Template.songItem
-			}]
-		};
-	},
-	filteredSongs: function() {
-		return Songs.find({
-			artist: Session.get('selectedArtist')
-		});
-	}
+    settings: function() {
+        return {
+            position: "top",
+            limit: 5,
+            rules: [{
+                collection: 'Songs',
+                subscription: 'autocompleteArtists',
+                field: "artist",
+                template: Template.songItem
+            }]
+        };
+    },
+    filteredSongs: function() {
+        return Songs.find({
+            artist: Session.get('selectedArtist')
+        });
+    }
 });
 
 Template.home.events({
-	"autocompleteselect input": function(event, template, doc) {
-		console.log("selected ", doc);
-		template.find('#artistInput').value = doc.artist;
-		Meteor.subscribe('artistSongs', doc.artist);
-		Session.set('selectedArtist', doc.artist);
-	},
-	"click a": function(event, template, doc) {
-		Materialize.toast('Ai votat!', 4000);
-	}
+    "autocompleteselect input": function(event, template, doc) {
+        event.preventDefault();
+        template.find('#artistInput').value = doc.artist;
+        Meteor.subscribe('artistSongs', doc.artist);
+        Session.set('selectedArtist', doc.artist);
+    },
+    "click li.song": function(event, template, doc) {
+        event.preventDefault();
+        var song = this;
+        Materialize.toast('Ai votat: ' + song.title, 4000);
+    }
 });
